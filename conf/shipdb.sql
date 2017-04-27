c7b0c490c6bd0344f3e7add0afc35ee242b393d5
wangxin <wangxin@trustedtech.com.cn>
2017-04-27 17:36:21 +0800

    create table

--- conf/shipdb.sql
+++ conf/shipdb.sql
@@ -1,6 +1,9 @@
 USE shipdb;
 
 DROP TABLE IF EXISTS `ship_user` CASCADE;
+DROP TABLE IF EXISTS `ship_node_nic` CASCADE;
+DROP TABLE IF EXISTS `ship_node_addr` CASCADE;
+DROP TABLE IF EXISTS `ship_route` CASCADE;
 
 CREATE TABLE `ship_user` (
   `id`       INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -9,4 +12,33 @@ CREATE TABLE `ship_user` (
   `is_admin` BOOL         NOT NULL
 );
 INSERT INTO `ship_user` (`name`, `password`, `is_admin`)
-VALUES ('admin', '2660fd05954ed585a9b3b43ec589ee018f0e211ecb7e474e5ee369d0785d0bea', TRUE);
+VALUES ('admin', 'fc9a0fbd8bcb2e5a50e629163886ce78b45372bd6e3bfe22b6b051a44e3d6c25', TRUE);
+
+CREATE TABLE `ship_node_nic` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `node_id` INT NOT NULL,
+  `name` VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE `ship_node_addr` (
+  `id`     INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nic_id` INT         NOT NULL REFERENCES ship_node_nic(id),
+  `ip`     VARCHAR(20) NOT NULL,
+  `mask`   VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE `ship_route` (
+  `id`      INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `subnet`  VARCHAR(20) NOT NULL,
+  `mask`    VARCHAR(20) NOT NULL,
+  `nic_id`  INT         NOT NULL REFERENCES ship_node_nic(id),
+  `gateway` VARCHAR(20)
+);
+
+CREATE TABLE `ship_man_addr` (
+  `id`      INT         NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nic`     VARCHAR(20) NOT NULL,
+  `ip`      VARCHAR(20) NOT NULL,
+  `mask`    VARCHAR(20) NOT NULL,
+  `gateway` VARCHAR(20) NOT NULL DEFAULT ''
+);
